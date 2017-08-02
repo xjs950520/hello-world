e59e1dbdb56451b02c4c2a07236bcbf88538cc57
xujunshuai <13162235587@163.com>
2017-08-02 18:14:08 +0800

    create and upload a project:hello-world

---
+++ src/com/aaa/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.aaa.demo;
+
+/**
+ * Created by xujunshuai on 2017/7/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+       System.out.println("Hello World");
+    }
+}

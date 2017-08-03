49ca0542cc5e7aa12a5f8f9ec224625d1e1ec53b
xujunshuai <13162235587@163.com>
2017-08-03 10:07:18 +0800

    create a new file

---
+++ src/com/aaa/demo/HelloWorld2.java
@@ -0,0 +1,10 @@
+package com.aaa.demo;
+
+/**
+ * Created by xujunshuai on 2017/8/3.
+ */
+public class HelloWorld2 {
+    public static void main(String[] args){
+        System.out.print("你好！");
+    }
+}

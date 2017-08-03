cdd57ace29e8a34a76cd4a8cd6e6e828069f22ea
xujunshuai <13162235587@163.com>
2017-08-03 10:12:04 +0800

    create a new class

---
+++ src/com/aaa/demo/HelloWorld3.java
@@ -0,0 +1,10 @@
+package com.aaa.demo;
+
+/**
+ * Created by xujunshuai on 2017/8/3.
+ */
+public class HelloWorld3 {
+    public static void main(String[] args){
+        System.out.print("Failure is the mother of success!");
+    }
+}
